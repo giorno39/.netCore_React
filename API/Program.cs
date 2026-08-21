94126a6afdb728ecf710d6831b787f7ea9dbc779
giorno39 <garcovi.izzi@gmail.com>
2026-08-22 00:51:39 +0300

    add initial react vite project

--- API/Program.cs
+++ API/Program.cs
@@ -11,10 +11,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 app.UseAuthorization();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
