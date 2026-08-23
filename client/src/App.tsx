dd51ed384fc6ccb279af58d814321f2f3645fd12
giorno39 <garcovi.izzi@gmail.com>
2026-08-23 15:12:52 +0300

    add material ui design and start using a local https connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,27 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { List, ListItem, ListItemText, Typography } from '@mui/material'
+import axios from 'axios'
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
 
   useEffect(() => {
-    fetch('https://localhost:5001/api/activities')
-      .then(response => response.json())
-      .then(data => setActivities(data))
+    axios.get<Activity[]>('https://localhost:5001/api/activities')
+      .then(response => setActivities(response.data))
   }, [])
 
   return (
-    <div>
-      <h3 className='app' style={{color: 'red'}} >Reactivities</h3>
-      <ul>
+    <>
+      <Typography variant='h3'>Reactivities</Typography>
+      <List>
           {activities.map((activity) => (
-            <li key={activity.id}>{activity.title}</li>
+            <ListItem key={activity.id}>
+              <ListItemText>{activity.title}</ListItemText>
+            </ListItem>
           ))}
-      </ul>
-    </div>
+      </List>
+    </>
   )
 }
 
