410693733a69b7c97e915ebbf76731d16cebba4c
giorno39 <garcovi.izzi@gmail.com>
2026-08-27 22:44:38 +0300

    add componenets for dashboards, forms, details

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,29 +1,54 @@
 import { useEffect, useState } from 'react'
-import { List, ListItem, ListItemText } from '@mui/material'
+import { Box, Container} from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline';
 import axios from 'axios'
-import NavBar from './navbar'
+import NavBar from './NavBar'
+import ActivityDashboard from '../../features/activities/DashBoard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined)
+  const [editMode, setEditMode] = useState(false)
 
   useEffect(() => {
     axios.get<Activity[]>('https://localhost:5001/api/activities')
       .then(response => setActivities(response.data))
   }, [])
 
+const handleSelectActivity = (id: string) => {
+  setSelectedActivity(activities.find(x => x.id === id))
+}
+
+const handleCanceSelectActivity = () => {
+  setSelectedActivity(undefined)
+}
+
+const handleOpenForm = (id?: string) => {
+  if(id) handleSelectActivity(id)
+  else handleCanceSelectActivity()
+  setEditMode(true)
+}
+
+const handleFormClose = () => {
+  setEditMode(false)
+}
+
   return (
-    <>
+    <Box sx={{bgcolor: "#eeeeee"}}>
       <CssBaseline/>
-      <NavBar/>
-      <List>
-          {activities.map((activity) => (
-            <ListItem key={activity.id}>
-              <ListItemText>{activity.title}</ListItemText>
-            </ListItem>
-          ))}
-      </List>
-    </>
+      <NavBar openForm={handleOpenForm}/>
+      <Container maxWidth='xl' sx={{mt: 3}}>
+        <ActivityDashboard 
+        activities={activities}
+        selectActivity={handleSelectActivity}
+        cancelSelectActivity={handleCanceSelectActivity}
+        selectedActivity={selectedActivity}
+        editMode={editMode}
+        openForm={handleOpenForm}
+        closeForm={handleFormClose}
+        />
+      </Container>
+    </Box>
   )
 }
 
--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,25 +1,39 @@
-import { Menu } from "@mui/icons-material";
-import { Box, AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material";
+import { Box, AppBar, Toolbar, Container, Typography, Button } from "@mui/material";
+import { Group } from "@mui/icons-material";
 
-export default function NavBar() {
+const navItemStyles = {
+  fontSize: '1.2rem',
+  textTransform: "uppercase",
+  fontWeight: "bold",
+  px: 2,
+  py: 1,
+  cursor: "pointer",
+  '&:hover': { opacity: 0.8 }
+};
+
+type Props ={
+  openForm: () => void
+}
+
+export default function NavBar({openForm}: Props) {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <Menu/>
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+      <AppBar position="static" sx={{backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)'}}>
+        <Container maxWidth='xl'>
+          <Toolbar sx={{display: 'flex', justifyContent: "space-between"}}>
+              <Box sx={{display: "flex", alignItems: "center", gap: 2}}>
+                <Group fontSize="large"/>
+                <Typography variant="h4" sx={{fontWeight: 'bold'}}>Reactivities</Typography>
+              </Box>
+              <Box sx={{display: "flex"}}>
+                <Box sx={navItemStyles}>Activities</Box>
+                <Box sx={navItemStyles}>About</Box>
+                <Box sx={navItemStyles}>Contact</Box>
+              </Box>
+              <Button onClick={openForm} size="large" variant="contained" color="warning">Create Activity</Button>
+          </Toolbar>
+        </Container>
+
       </AppBar>
     </Box>
   )
